7c5cdf72e70954896b984a60358f3f6a66f06750
Christopher Wetherill <git@tbmh.org>
2023-12-23 10:22:58 -0500

    migration from dstk-labs/dstk

---
+++ src/pydstk/__init__.py
@@ -0,0 +1,8 @@
+# Package versioning should conform to PEP 440
+#
+# Specifically, this project uses the syntax
+# X.Y.Z-a
+# where `X` is major version, `Y` is minor version,
+# `Z` is patch version, and `a` optionally denotes
+# pre-release version from <alpha|beta|rc>
+__version__ = "0.1.0-alpha"
